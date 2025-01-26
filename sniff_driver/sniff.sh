b294c01456ef05c877829658fb520a799d70eb4c
Robert Strzoda <roberts.wp.pl@wp.pl>
2025-01-26 21:33:52 +0100

    bash script to use driver

---
+++ sniff_driver/sniff.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+SNIFF_DIR="sniff"
+
+
+if [ ! -d "$SNIFF_DIR" ]; then
+    echo "Directory $SNIFF_DIR does not exist."
+    exit 1
+fi
+
+
+cd "$SNIFF_DIR"
+
+
+echo "Starting the packet sniffer on interface sdr0..."
+sudo ./packet_sniffer sdr0
